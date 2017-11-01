06abf5b8aa4ca842a01090f392d14133546debf7
Chelsea Bergman <chelseacbergman@gmail.com>
2017-11-01 13:19:06 -0400

    hide-show pages

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    console.log("Hi");
+
+
+    $("#startButton").on("click", function () {
+        $("#startGamePage").css("display", "none");
+        $("#gamePage").fadeIn("slow").css("display", "block");
+
+    });
+
+    $("#resetButton").on("click", function () {
+        $("#gamePage").css("display", "none");;
+        $("#startGamePage").fadeIn("slow").css("display", "block");
+    });
+
+});
