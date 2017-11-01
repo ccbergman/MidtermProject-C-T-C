8e343c8b35ef2c34862c6ec289b41ddb19767979
Chelsea Bergman <chelseacbergman@gmail.com>
2017-11-01 16:27:29 -0400

    problems, man.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,28 @@
 $(document).ready(function () {
-   console.log("Hi");
+    console.log("Hi");
 
+    var cardClicked;
 
-   $("#startButton").on("click", function () {
-       $("#startGamePage").css("display", "none");
-       $("#gamePage").fadeIn("slow").css("display", "block");
+    $("#startButton").on("click", function () {
+        $("#startGamePage").css("display", "none");
+        $("#gamePage").fadeIn("slow").css("display", "block");
 
-   });
+    });
 
-   $("#resetButton").on("click", function () {
-       $("#gamePage").css("display", "none");;
-       $("#startGamePage").fadeIn("slow").css("display", "block");
-   });
+    $("#resetButton").on("click", function () {
+        $("#gamePage").css("display", "none");;
+        $("#startGamePage").fadeIn("slow").css("display", "block");
+    });
 
-   // $(function () {
-   //     $("#card1").flip({
-   //         trigger: 'click'
-   //     });
-   //     $(".back").css("display", "none");
-   //     $(".front").css("display", "block");
-   // });
+    // $(function () {
+    //     $("card1").flip({
+    //         trigger: 'click'
+    //     });
+    //     $(".front").css("display", "block");
+    //     $(".back").css("display", "none");
+    // });
 
-   $('#resetButton').click(function () {
-       location.reload();
-   });
+    $('#resetButton').click(function () {
+        location.reload();
+    });
 });
