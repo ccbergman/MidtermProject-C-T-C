cd690f657bf0f63c97377c6a70843d3ba7948e56
Tamara Trotter <tltrotte@me.com>
2017-11-01 16:07:29 -0400

    removed buttons and added divs instead for cards

--- scripts.js
+++ scripts.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
-    console.log("Hi");
+   console.log("Hi");
 
 
-    $("#startButton").on("click", function () {
-        $("#startGamePage").css("display", "none");
-        $("#gamePage").fadeIn("slow").css("display", "block");
+   $("#startButton").on("click", function () {
+       $("#startGamePage").css("display", "none");
+       $("#gamePage").fadeIn("slow").css("display", "block");
 
-    });
+   });
 
-    $("#resetButton").on("click", function () {
-        $("#gamePage").css("display", "none");;
-        $("#startGamePage").fadeIn("slow").css("display", "block");
-    });
+   $("#resetButton").on("click", function () {
+       $("#gamePage").css("display", "none");;
+       $("#startGamePage").fadeIn("slow").css("display", "block");
+   });
 
-    // $(function () {
-    //     $("#card1").flip({
-    //         trigger: 'click'
-    //     });
-    //     $(".back").css("display", "none");
-    //     $(".front").css("display", "block");
-    // });
+   // $(function () {
+   //     $("#card1").flip({
+   //         trigger: 'click'
+   //     });
+   //     $(".back").css("display", "none");
+   //     $(".front").css("display", "block");
+   // });
 
-    $('#resetButton').click(function () {
-        location.reload();
-    });
+   $('#resetButton').click(function () {
+       location.reload();
+   });
 });
