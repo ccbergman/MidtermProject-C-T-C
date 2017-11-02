130e693f454ee5566ec6139fe079e002e9e40d54
Chelsea Bergman <chelseacbergman@gmail.com>
2017-11-02 11:19:40 -0400

    shuffle arry working

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
 
-    var cardClicked;
-    var graphicsArray = ["images/aquaman.sm.jpg", "images/captainamerica.sm.jpg", "images/deadpool.sm.jpg",
+    var array = ["images/aquaman.sm.jpg", "images/captainamerica.sm.jpg", "images/deadpool.sm.jpg",
         "images/flash.sm.jpg", "images/ironman.sm.jpg", "images/spooooderman.sm.jpg", "images/aquaman.sm.jpg", "images/captainamerica.sm.jpg", "images/deadpool.sm.jpg",
         "images/flash.sm.jpg", "images/ironman.sm.jpg", "images/spooooderman.sm.jpg"];
-
 
     $("#startButton").on("click", function () {
         $("#startGamePage").css("display", "none");
@@ -14,6 +12,26 @@ $(document).ready(function () {
     $('#resetButton').click(function () {
         location.reload();
     });
+
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
+    }
+
+    array = shuffle(array);
+    console.log(array);
+
+
+
+
+
 
     // $(function () {
     //     $(".card").flip({
